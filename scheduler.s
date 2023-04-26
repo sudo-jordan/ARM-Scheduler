fa009179cc7e585dff5926655084996bbcff4d99
William Danny Vasquez <112425665+The13thWatcher@users.noreply.github.com>
2023-04-26 04:05:50 -0400

    Update scheduler.s
    Flex option mostly added

--- scheduler.s
+++ scheduler.s
@@ -138,6 +138,88 @@ clear: @clear time out
 	bl sundayClear
 	b main
 	
+flex: @Add an event, flexible time
+	ldr r0, =time_length
+        bl printf
+        ldr r0, =format_select
+        ldr r1, =select_buff
+	bl scanf
+        ldr r1, =select_buff
+        ldr r1, [r1]
+	mov r9, #0 @set start time at 0
+	mov r10, r1 @copy input into r10
+	bl createTimeSlot @Fully right-shifted time slot in r11
+	mov r0, #0
+	b flexCheck
+	
+flexCheck: @Check the first time slot for each day, attempting an edit. If none found, lsl r11 and check next time slot. Loop until all slots checked.
+	cmp r0, #23
+	ldreq r0, =no_time
+	bleq printf
+        beq main
+	
+	bl mondayEdit @Attempt to edit Monday
+	cmp r9, r2 @Check to see if r2 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_monday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	bl tuesdayEdit @Attempt to edit Tuesday
+	cmp r9, r3 @Check to see if r3 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_tuesday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	bl wednesdayEdit @Attempt to edit Wednesday
+	cmp r9, r4 @Check to see if r4 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_wednesday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	bl thursdayEdit @Attempt to edit Thursday
+	cmp r9, r5 @Check to see if r5 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_thursday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	bl fridayEdit @Attempt to edit friday
+	cmp r9, r6 @Check to see if r6 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_friday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	bl saturdayEdit @Attempt to edit Saturday
+	cmp r9, r7 @Check to see if r7 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_saturday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	bl sundayEdit @Attempt to edit Sunday
+	cmp r9, r8 @Check to see if r8 has been edited
+	moveq r12, r0 @save the count to r12 for safekeeping
+	ldreq r0, =flex_sunday @Confirm placement
+	bleq printf
+	@Add in a print of r12 here, so that it will say what time slot the event was placed in. Make sure you add eq to the end of every instruction
+	beq main
+	
+	adds r0, #1 @Increment time slot counter
+	lsl r11, #1 @If no open time slots found, shift one time slot over
+	b flexCheck
+
+schedule: @Print a portion of the schedule
+	
 mondayEdit:
 	push {lr}
 	mov r9, r2 @Setup for check
@@ -238,10 +320,6 @@ overlapCheck: @Check to ensure there is no overlap
 	eor r10, r11
 	cmp r12, r10 @Exclusive or and or should be equal if there is no overlap
 	bx lr
-	
-flex: @Add an event, flexible time
-
-schedule: @Print a portion of the schedule
 
 help: @Display help
 	ldr r0, =full_help
@@ -268,5 +346,13 @@ exit: @Exit the program
 	select:		.asciz "\n\nSelect a menu option> "
 	done:		.asciz "\nExiting!"
 	full_help:	.asciz "\nWhen choosing a day, input a number 1-7, with Monday being equal to 1.\nWhen choosing a time, please input the hour only, and use military time.\nNo change will be made if there is overlap or if there is no possible way to fit a flexible event."
+	no_time:	.asciz "\nNo time slots found for this event."
+	flex_monday	.asciz "\nThis event has been placed on Monday at time slot "
+	flex_tuesday	.asciz "\nThis event has been placed on Tuesday at time slot "
+	flex_wednesday	.asciz "\nThis event has been placed on Wednesday at time slot "
+	flex_thursday	.asciz "\nThis event has been placed on Thursday at time slot "
+	flex_friday	.asciz "\nThis event has been placed on Friday at time slot "
+	flex_saturday	.asciz "\nThis event has been placed on Saturday at time slot "
+	flex_sunday	.asciz "\nThis event has been placed on Sunday at time slot "
 	format_select:	.asciz "%d"
 	select_buff:	.word 0
